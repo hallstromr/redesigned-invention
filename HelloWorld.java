13040836bd5bd7950d468fcc39f5fd38fdf5e057
Reilly_Hallstrom <hallstromr@carleton.edu>
2017-10-07 22:29:14 -0500

    Added a little more for example

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld
 		// here is where we get the number that the user types in
 		int i = sc.nextInt();
 
-		// here is where we figure out how many of each coin is included.		
+		// here is where we figure out how many of each coin is included.
 		int q = 25;
 		int d = 10;
 		int quarters = (i/q);
@@ -26,13 +26,22 @@ public class HelloWorld
 		*/
 
 		System.out.println("Hello Earthling! \nWelcome to Reilly, Geoff, and Lason's temperature converter");
-		
+
 		// Opens a scanner which will be used to get the value the user types into the command line
 		Scanner sc = new Scanner (System.in);
 		System.out.print("Enter a temperature:\n");
 
 		// here is where we get the number that the user types in
-		int i = sc.nextInt();
+		String entry = sc.nextLine();
+		int temp = Integer.parseInt(entry);
+
+		System.out.print("Enter a temperature unit (F or C):\n");
+		String unit = sc.nextLine();
+
+		if (unit.equals("C")) {
+			System.out.println("Here");
+			//convert to farenheit
+		}
 
 	}
 }
