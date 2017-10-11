ad5b66ac9a162e19521808aa2302f6c0565f5f61
Geoff Nelson <geoff.nel16@gmail.com>
2017-10-10 21:33:23 -0700

    Fixes #7:Completed Temp Converter

--- TempConverter.java
+++ TempConverter.java
@@ -1,4 +1,5 @@
-/* HelloWorld.java
+/*
+HelloWorld.java
  */
 import java.util.Scanner;
 
@@ -9,17 +10,38 @@ public class TempConverter
 
 		// Opens a scanner which will be used to get the value the user types into the command line
 		Scanner sc = new Scanner (System.in);
-		System.out.print("Enter a temperature:\n");
+		System.out.println("Enter a temperature:");
 
 		// here is where we get the number that the user types in
 		String entry = sc.nextLine();
 		int temp = Integer.parseInt(entry);
 
-		System.out.print("Enter a temperature unit (F or C):\n");
+		System.out.println("Enter a temperature unit (F or C):");
 		String unit = sc.nextLine();
 
+		double convertedTemp = 0.00;
+		boolean tempIsConverted = false;
+		boolean fahrenheit = false;
+		boolean celsius = false;
+
 		if (unit.equals("C")) {
-			//convert to farenheit
+			convertedTemp = (temp*1.8)+32;
+			tempIsConverted = true;
+			celsius = true;
+		}
+		else if (unit.equals("F")) {
+			convertedTemp = (temp-32)/1.8;
+			tempIsConverted = true;
+			fahrenheit = true;
+		}
+		else {
+			System.out.println("Only able to use F or C");
+		}
+		if (tempIsConverted && fahrenheit) {
+			System.out.println("The Converted Temp is: " + convertedTemp + " Celsius");
+		}
+		if (tempIsConverted && celsius) {
+			System.out.println("The Converted Temp is: " + convertedTemp + " Fahrenheit");
 		}
 	}
 }
