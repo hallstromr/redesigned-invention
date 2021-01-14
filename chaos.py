675bfe1427f7862c01406eced1f1b7967816ad5b
Elliot <elliot@thermlink.com>
2021-01-14 16:22:36 -0600

    Updating Files to hande Python 3

--- chaos.py
+++ chaos.py
@@ -2,14 +2,14 @@
 # Asimple program illustratin chaotic behavior
 
 def main():
-    print "This program illustrates a chaotic function"
+    print("This program illustrates a chaotic function")
     n = input("How many numbers should I print: ")
     x = input("Enter a number between 0 and 1: ")
     y = input("Enter a number between 0 and 1: ")
     for i in range(n):
         x = 3.9 * x * (1-x)
         y = 3.9 * y * (1-y)
-        print x
-        print y
+        print(x)
+        print(y)
 
 main()
